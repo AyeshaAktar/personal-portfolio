5008fe5df8643996c92acab23bf9f3396dd0f0bc
Ayesha Akter <ayeshaaktar491@gmail.com>
2021-11-30 22:56:36 +0600

    detail-1

--- src/pages/Details/Detail1.js
+++ src/pages/Details/Detail1.js
@@ -1,9 +1,53 @@
 import React from "react";
+import "./Details.css";
+import carGallery from "../../images/car-gallery.JPG";
+import products from "../../images/car-products.JPG";
+import dashboard from "../../images/car-dashboard.JPG";
 
 const Detail1 = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Car Gallery</h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        <div className="col">
+          <div className="card">
+            <img className="image" src={carGallery} alt="" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img className="image" src={products} alt="" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img className="image" src={dashboard} alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="text-start my-5">
+        <ul>
+          <li>
+            This website is about to car selling. In this website user can buy a
+            car via online.
+          </li>
+          <li>
+            Admin can manage whole website but user can only manage orders and
+            review products..
+          </li>
+          <li>
+            In authentication system use email & password for login/signup also
+            use google login.
+          </li>
+          <li>
+            Using Bootstrap, CSS3, React, Firebase, MongoDB, Node Js, Express
+            Js, Heroku.
+          </li>
+        </ul>
+        <a href="">
+          <button className="btn btn-secondary">Code Link</button>
+        </a>
+      </div>
     </div>
   );
 };
